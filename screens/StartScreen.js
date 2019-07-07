4d029680082deb5c6066196c02497052ca9da618
seokwon <swlee1014@gmail.com>
2019-07-07 10:57:58 +0900

    화면 전환문제 해결, 웹말고 핸드폰으로 케이블 꽂으니 된다.

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {ImageBackground, TouchableHighlight, Alert} from 'react-native';
+import backgroundImg from '../assets/images/download.jpeg';
 
 export default class StartScreen extends React.Component {
-  render(){
-    return(
-      <Image
-          source={require('../assets/images/download.jpeg')}
-        />
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+  render() {
+    return (
+      <TouchableHighlight
+      onPress={() => this.props.navigation.navigate("Main")} 
+      underlayColor="white">
+        <ImageBackground
+        source={backgroundImg}
+        style={{width: '100%', height: '100%'}}>
+        </ImageBackground>
+      </TouchableHighlight>
     );
   }
 }
