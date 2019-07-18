20522d1e6f331ca3b41da6314c0691fdde8cf321
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2019-07-18 12:46:13 +0900

    Carousel 적용
    - react-native-snap-carousel 설치 및 적용

--- constants/Colors.js
+++ constants/Colors.js
@@ -12,3 +12,10 @@ export default {
   noticeBackground: tintColor,
   noticeText: '#fff',
 };
+
+export const colors = {
+  black: '#1a1917',
+  gray: '#888888',
+  background1: '#B721FF',
+  background2: '#21D4FD',
+}
