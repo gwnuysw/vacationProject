c7b6f7b37dde247154c8a52a2a02c149a945323b
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2019-07-22 11:27:57 +0900

    Marker

--- components/Map.js
+++ components/Map.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Content, View, Container } from 'native-base';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class Map extends React.Component {
-    render() {
-        return (
-            <MapView
-                provider={PROVIDER_GOOGLE}
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                }}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-            />
-        );
-    }
+  render() {
+    return (
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          title="This is a title"
+          description="This is a description"
+          coordinate={{latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,}}
+        />
+      </MapView>
+    );
+  }
 }
